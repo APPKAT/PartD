8e37341064bc930e6cb10c60de396579c6389431
kat <kat@localhost>
2021-01-13 15:10:20 +1300

    Line 4 - Weekday

--- GitHub - Part D/src/GitHub.java
+++ GitHub - Part D/src/GitHub.java
@@ -7,6 +7,7 @@ public class GitHub {
 		System.out.println("Hi, my name is Katharina Vogl.");
 		System.out.println("My favourite colour is blue.");
 		System.out.println("My favourite mode of transportation is by bike.");
+		System.out.println("The best day of the week is Friday.");
 	}
 
 }
