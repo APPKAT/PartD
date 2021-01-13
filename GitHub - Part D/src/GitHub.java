b68851dc259205928df6494a5dec5150f930d09d
kat <kat@localhost>
2021-01-13 15:08:27 +1300

    Line 3 - Transportation

--- GitHub - Part D/src/GitHub.java
+++ GitHub - Part D/src/GitHub.java
@@ -6,6 +6,7 @@ public class GitHub {
 		
 		System.out.println("Hi, my name is Katharina Vogl.");
 		System.out.println("My favourite colour is blue.");
+		System.out.println("My favourite mode of transportation is by bike.");
 	}
 
 }
